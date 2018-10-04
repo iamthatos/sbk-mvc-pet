3e62aae69b40e4d901238d4984d80873ec0a8f80
Thato Seboko <thato.seboko@gmail.com>
2018-10-04 15:18:47 +0200

    initial project commit

---
+++ src/main/java/com/sbk/sbkmvcpet/SbkMvcPetApplication.java
@@ -0,0 +1,12 @@
+package com.sbk.sbkmvcpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbkMvcPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbkMvcPetApplication.class, args);
+	}
+}
