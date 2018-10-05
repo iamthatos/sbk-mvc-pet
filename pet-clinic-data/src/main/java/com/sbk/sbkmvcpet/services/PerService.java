685bbb5e37339b55680b908677c62f7ecba2e7f4
Thato Seboko <thato.seboko@gmail.com>
2018-10-05 16:51:45 +0200

    created common crud service . Closes #11.

--- pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/services/PerService.java
+++ pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/services/PerService.java
@@ -2,14 +2,5 @@ package com.sbk.sbkmvcpet.services;
 
 import com.sbk.sbkmvcpet.model.Pet;
 
-import java.util.Set;
-
-public interface PerService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-
+public interface PerService extends CrudService<Pet, Long>  {
 }
--- pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/services/VetService.java
+++ pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/services/VetService.java
@@ -2,14 +2,6 @@ package com.sbk.sbkmvcpet.services;
 
 import com.sbk.sbkmvcpet.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
-
+public interface VetService extends CrudService<Vet, Long>  {
+    
 }
