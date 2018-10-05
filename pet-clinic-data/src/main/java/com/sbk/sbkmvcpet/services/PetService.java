097098744f14140514320d3e7e62b6df30adebf3
Thato Seboko <thato.seboko@gmail.com>
2018-10-05 17:21:54 +0200

    implemented map to store data . Closes #6.

--- pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/services/PetService.java
+++ pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/services/PetService.java
@@ -2,5 +2,5 @@ package com.sbk.sbkmvcpet.services;
 
 import com.sbk.sbkmvcpet.model.Pet;
 
-public interface PerService extends CrudService<Pet, Long>  {
+public interface PetService extends CrudService<Pet, Long>  {
 }
