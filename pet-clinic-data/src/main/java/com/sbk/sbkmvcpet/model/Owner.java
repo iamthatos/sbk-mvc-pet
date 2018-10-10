0f431a7836f71bd4b2cb8f93c991bc4d04bd76a2
Thato Seboko <thato.seboko@gmail.com>
2018-10-10 17:04:37 +0200

    Added Project Lombok to project, closes #51

--- pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/model/Owner.java
+++ pet-clinic-data/src/main/java/com/sbk/sbkmvcpet/model/Owner.java
@@ -1,9 +1,17 @@
 package com.sbk.sbkmvcpet.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -19,36 +27,15 @@ public class Owner extends Person {
 
     @Column(name = "telephone")
     private String telephone;
-    
-    public Set<Pet> getPets() {
-        return pets;
-    }
-    
-    public void setPets(Set<Pet> pets) {
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
         this.pets = pets;
     }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+    
 }
